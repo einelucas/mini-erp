d9966d36306c7a7ca63b2afcc08e207066a78f1c
einelucas <lucaspinheiro863@gmail.com>
2026-02-17 15:17:52 -0400

    Atualizando URL da API para produção

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://mini-erp-98tn.onrender.com";
 
 // ===== LOGIN =====
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
